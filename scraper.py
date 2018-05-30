24b6114931477ab9dbd219f0d4f57d60696d53bf
blablupcom <o.volodin@yahoo.com>
2018-05-30 18:05:58 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,6 +9,7 @@ import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+
 #### FUNCTIONS 1.0
 
 def validateFilename(filename):
@@ -81,52 +82,44 @@ def convert_mth_strings ( mth_string ):
         mth_string = mth_string.replace(k, v)
     return mth_string
 
+
 #### VARIABLES 1.0
 
-entity_id = "E1821_WCC_gov"
-url = "http://www.worcestershire.gov.uk/info/20024/council_finance/331/payments_to_commercial_suppliers_over_500_and_government_procurement_card_transactions"
+entity_id = "E3831_ADC_gov"
+url = "https://www.adur-worthing.gov.uk/about-the-councils/open-data/payments-to-suppliers/"
 errors = 0
 data = []
+
 
 #### READ HTML 1.0
 
 html = urllib2.urlopen(url)
 soup = BeautifulSoup(html, 'lxml')
 
-
 #### SCRAPE DATA
 
-block = soup.find('div',{'id':'content'})
-links = block.findAll('a', text=re.compile('View Reports'))
-
+links = soup.find('ul', attrs = {'class': 'jump-menu-see-also'}).find_all('a')
 for link in links:
     if 'http' not in link['href']:
-        suburl = 'http://www.worcestershire.gov.uk' + link['href']
+        year_url = 'https://www.adur-worthing.gov.uk'+link['href']
     else:
-        suburl = link['href']
-    if 'payments_to_commercial_suppliers_over' in suburl:
-        html2 = urllib2.urlopen(suburl)
-        soup2 = BeautifulSoup(html2, 'lxml')
-        block = soup2.find('ul', {'class':'item-list item-list__rich'})
-        sublinks = block.findAll('a', href=True)
-        for sublink in sublinks:
-            filePageUrl = sublink['href']
-            title = sublink.encode_contents(formatter='html').replace('&nbsp;',' ')
-            title = title.upper().strip()
-            html3 = urllib2.urlopen(filePageUrl)
-            soup3 = BeautifulSoup(html3, 'lxml')
-            block = soup3.find('main',{'class':'main-content'})
-            filelinks = block.findAll('a', href=True)
-            for filelink in filelinks:
-                fileurl = filelink['href']
-                if 'Download' in filelink.text:
-                    csvYr = title.split(' ')[-1]
-                    csvMth = title.split(' ')[-2][:3]
-                    if ' - ' not in title:
-                        csvYr = title.split()[1]
-                        csvMth = title.split()[0][:3]
-                    csvMth = convert_mth_strings(csvMth)
-                    data.append([csvYr, csvMth, fileurl])
+        year_url = link['href']
+    year_html = urllib2.urlopen(year_url)
+    year_soup = BeautifulSoup(year_html, 'lxml')
+    blocks = year_soup.find_all('a', 'file-xls')
+    for block in blocks:
+        url = block['href']
+        if 'http' not in url:
+            url = 'https://www.adur-worthing.gov.uk'+url
+        else:
+            url = url
+        file_name = block.find('span', 'file-name').text
+        if 'Joint' in file_name:
+            csvYr = file_name.split('- ')[-1].split('(')[0].strip()[-4:]
+            csvMth = file_name.split('- ')[-1].split('(')[0].strip()[:3]
+            csvMth = convert_mth_strings(csvMth.upper())
+            data.append([csvYr, csvMth, url])
+
 
 #### STORE DATA 1.0
 
